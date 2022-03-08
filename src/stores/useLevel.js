5ec5281c2ea2bbba03e98b7815eb071ec93073ec
Momavi <68790545+Momavi@users.noreply.github.com>
2022-03-09 00:29:58 +0300

    create logic to elevator

--- src/stores/useLevel.js
+++ src/stores/useLevel.js
@@ -13,5 +13,40 @@ export const useLevelStore = defineStore("level", {
     elevatorCall(level) {
       this.elevatorCallArray.push(level);
     },
+    moveElevator(nextLevel) {
+      this.elevatorMoving = true;
+      // Проверка если нажали на том же этаже где находится лифт
+      if (nextLevel === this.elevatorLevel) {
+        this.elevatorCallArray.shift();
+        this.elevatorMoving = false;
+      }
+      if (this.elevatorLevel < nextLevel && nextLevel) {
+        // цикл на подьем лифта
+        let j = 1;
+        for (let i = this.elevatorLevel; i < nextLevel; i++) {
+          j++;
+          setTimeout(() => {
+            this.$patch((state) => state.elevatorLevel++);
+            if (this.elevatorLevel === nextLevel) {
+              this.elevatorCallArray.shift();
+              this.elevatorMoving = false;
+            }
+          }, j * ELEVATOR_SPEED);
+        }
+      } else {
+        // цикл на опускание лифта
+        let j = 1;
+        for (let i = this.elevatorLevel; i > nextLevel; i--) {
+          j++;
+          setTimeout(() => {
+            this.$patch((state) => state.elevatorLevel--);
+            if (this.elevatorLevel === nextLevel) {
+              this.elevatorCallArray.shift();
+              this.elevatorMoving = false;
+            }
+          }, j * ELEVATOR_SPEED);
+        }
+      }
+    },
   },
 });
