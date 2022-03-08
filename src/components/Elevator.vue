5f804a213b4ae5b441d8b78c4304ce19f1c6e3ff
Momavi <68790545+Momavi@users.noreply.github.com>
2022-03-09 00:26:33 +0300

    change styles

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -18,7 +18,6 @@
   .wrapper {
     margin: 0 auto;
     padding: 1px;
-    color: black;
     border-top: 2px solid black;
   }
   .wrapper:first-child {
@@ -32,23 +31,33 @@
     align-items: center;
   }
 
+  .elevator {
+    border: 2px solid rgb(233, 233, 233);
+    width: 50px;
+    height: 50px;
+  }
+
+  .elevator-offline {
+    background-color: rgb(117, 110, 110);
+  }
   .level_wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
     align-content: center;
+    margin-left: 10px;
+    width: 30px;
+    height: 62px;
   }
   .level__number {
     border: 1px solid;
     border-color: transparent;
     border-radius: 30px;
     font-size: 16px;
-    padding: 10px 5px;
-    margin: 5px 5px;
   }
   .level__btn {
-    border: 2px solid rgba(78, 226, 9, 0.842);
+    border: 2px solid rgba(83, 245, 8, 0.945);
     border-radius: 20px;
     color: rgb(150, 150, 150);
     padding: 12px 12px;
@@ -58,6 +67,10 @@
     font-size: 16px;
     cursor: pointer;
     transition: all 0.35s;
+  }
+  .level__btn-offline {
+    border: 2px solid rgba(221, 236, 6, 0.884);
+    cursor: auto;
   }
   .level__btn:hover {
     border: 2px solid rgba(237, 253, 5, 0.952);
