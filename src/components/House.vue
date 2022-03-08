e638e40342c250fd8194ff5bbcca371a4d8b6fdc
Momavi <68790545+Momavi@users.noreply.github.com>
2022-03-09 00:28:07 +0300

    Update House.vue

--- src/components/House.vue
+++ src/components/House.vue
@@ -3,15 +3,12 @@
 </script>
 
 <template>
-  <div class="elevator">
   <div class="page">
     <Elevator />
   </div>
 </template>
 
 <style>
-  .elevator {
-    background: #fff;
   .page {
     background: rgba(75, 73, 73, 0.466);
     border: 2px solid transparent;
