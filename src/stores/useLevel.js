b4845597eb47e49834477fd0e205215aff2b7947
Momavi <68790545+Momavi@users.noreply.github.com>
2022-03-10 21:14:14 +0300

    Added logic

--- src/stores/useLevel.js
+++ src/stores/useLevel.js
@@ -7,44 +7,59 @@ export const useLevelStore = defineStore("level", {
     elevatorChill: false,
     elevatorLevel: 1,
     elevatorCallArray: [],
+    elevatorMoveToLevel: {
+      direction: null,
+      level: null,
+    },
   }),
   getters: {},
   actions: {
     elevatorCall(level) {
       this.elevatorCallArray.push(level);
     },
-    moveElevator(nextLevel) {
+    setElevatorMoveToLevel(direction, level) {
+      this.elevatorMoveToLevel.direction = direction;
+      this.elevatorMoveToLevel.level = level;
       this.elevatorMoving = true;
+    },
+    stopElevator(nextLevel, timeout) {
+      if (this.elevatorLevel === nextLevel) {
+        this.elevatorCallArray.shift();
+        this.elevatorMoving = false;
+        this.elevatorChill = true;
+        setTimeout(() => {
+          this.elevatorChill = false;
+        }, timeout * ELEVATOR_SPEED + ELEVATOR_Chill - 2000);
+      }
+    },
+    moveElevator(nextLevel) {
       // Проверка если нажали на том же этаже где находится лифт
       if (nextLevel === this.elevatorLevel) {
         this.elevatorCallArray.shift();
         this.elevatorMoving = false;
-      }
-      if (this.elevatorLevel < nextLevel && nextLevel) {
-        // цикл на подьем лифта
-        let j = 1;
-        for (let i = this.elevatorLevel; i < nextLevel; i++) {
-          j++;
-          setTimeout(() => {
-            this.$patch((state) => state.elevatorLevel++);
-            if (this.elevatorLevel === nextLevel) {
-              this.elevatorCallArray.shift();
-              this.elevatorMoving = false;
-            }
-          }, j * ELEVATOR_SPEED);
-        }
       } else {
-        // цикл на опускание лифта
-        let j = 1;
-        for (let i = this.elevatorLevel; i > nextLevel; i--) {
-          j++;
-          setTimeout(() => {
-            this.$patch((state) => state.elevatorLevel--);
-            if (this.elevatorLevel === nextLevel) {
-              this.elevatorCallArray.shift();
-              this.elevatorMoving = false;
-            }
-          }, j * ELEVATOR_SPEED);
+        if (this.elevatorLevel < nextLevel && nextLevel) {
+          // цикл на подьем лифта
+          this.setElevatorMoveToLevel("🠕", nextLevel);
+          let j = 1;
+          for (let i = this.elevatorLevel; i < nextLevel; i++) {
+            j++;
+            setTimeout(() => {
+              this.$patch((state) => state.elevatorLevel++);
+              this.stopElevator(nextLevel, j);
+            }, j * ELEVATOR_SPEED);
+          }
+        } else {
+          // цикл на опускание лифта
+          this.setElevatorMoveToLevel("🠗", nextLevel);
+          let j = 1;
+          for (let i = this.elevatorLevel; i > nextLevel; i--) {
+            j++;
+            setTimeout(() => {
+              this.$patch((state) => state.elevatorLevel--);
+              this.stopElevator(nextLevel, j);
+            }, j * ELEVATOR_SPEED);
+          }
         }
       }
     },
